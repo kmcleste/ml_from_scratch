975b271e8cea42bede249fc538ff90726af76e23
Kyle McLester <kmcleste@uncc.edu>
2021-02-24 23:32:02 -0500

    This is a test commit

---
+++ knn.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+class KNN:
+
+    def __init__(self, k=3): # K = number of nearest neighbors to consider.
+        self.k = k
+
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
+
+    def predict(self, X):
+        predicted_labels = [self._predict(x) for x in X]
+        return np.array(predicted_labels)
+
+    def _predict(self, x):
+        pass
